a9eec87ce8593c6d334898d81372fc2b34622ae5
Anatoliy Seleznyov <anatoliy.seleznyov@gmail.com>
2015-04-03 12:13:24 +0300

    Added Geometry class to hold functions on geometry primitives

---
+++ Utils/Geometry.java
@@ -0,0 +1,20 @@
+package Utils;
+
+public class Geometry {
+  /*public static double cross_product(Vector a, Vector b) {
+    return 0;
+  }
+  
+  public static double scalar_product(Vector a, Vector b) {
+    return 0;
+  }
+
+  public static Point intersect(Segment a, Segment b) {
+    return new Point();
+  }
+
+  public static Point intersect(Line a, Line b) {
+    return new Point();
+  }*/
+
+}
